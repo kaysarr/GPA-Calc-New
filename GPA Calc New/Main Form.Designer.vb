c92dbcb1cc35a012fda618896ac99060c1677297
kadde5945 <kadde5945@W10-DRM2CS3>
2023-02-27 13:35:02 -0600

    Add project files.

---
+++ GPA Calc New/Main Form.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim lstGPA As System.Windows.Forms.ListBox
+        Me.radFemale = New System.Windows.Forms.RadioButton()
+        Me.radMale = New System.Windows.Forms.RadioButton()
+        Me.radNonBin = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblGPA = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        lstGPA = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'radFemale
+        '
+        Me.radFemale.AutoSize = True
+        Me.radFemale.Location = New System.Drawing.Point(6, 24)
+        Me.radFemale.Name = "radFemale"
+        Me.radFemale.Size = New System.Drawing.Size(74, 20)
+        Me.radFemale.TabIndex = 0
+        Me.radFemale.TabStop = True
+        Me.radFemale.Text = "Female"
+        Me.radFemale.UseVisualStyleBackColor = True
+        '
+        'radMale
+        '
+        Me.radMale.AutoSize = True
+        Me.radMale.Location = New System.Drawing.Point(6, 50)
+        Me.radMale.Name = "radMale"
+        Me.radMale.Size = New System.Drawing.Size(58, 20)
+        Me.radMale.TabIndex = 0
+        Me.radMale.TabStop = True
+        Me.radMale.Text = "Male"
+        Me.radMale.UseVisualStyleBackColor = True
+        '
+        'radNonBin
+        '
+        Me.radNonBin.AutoSize = True
+        Me.radNonBin.Location = New System.Drawing.Point(6, 76)
+        Me.radNonBin.Name = "radNonBin"
+        Me.radNonBin.Size = New System.Drawing.Size(95, 20)
+        Me.radNonBin.TabIndex = 0
+        Me.radNonBin.TabStop = True
+        Me.radNonBin.Text = "Non-Binary"
+        Me.radNonBin.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radMale)
+        Me.GroupBox1.Controls.Add(Me.radNonBin)
+        Me.GroupBox1.Controls.Add(Me.radFemale)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 38)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(133, 120)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Gender"
+        '
+        'lstGPA
+        '
+        lstGPA.FormattingEnabled = True
+        lstGPA.ItemHeight = 16
+        lstGPA.Location = New System.Drawing.Point(213, 50)
+        lstGPA.Name = "lstGPA"
+        lstGPA.Size = New System.Drawing.Size(67, 116)
+        lstGPA.TabIndex = 2
+        '
+        'lblGPA
+        '
+        Me.lblGPA.AutoSize = True
+        Me.lblGPA.Location = New System.Drawing.Point(210, 31)
+        Me.lblGPA.Name = "lblGPA"
+        Me.lblGPA.Size = New System.Drawing.Size(38, 16)
+        Me.lblGPA.TabIndex = 3
+        Me.lblGPA.Text = "GPA:"
+        Me.lblGPA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(343, 50)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(100, 32)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(343, 88)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 32)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(343, 126)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 32)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 354)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblGPA)
+        Me.Controls.Add(lstGPA)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "GPA Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents radFemale As RadioButton
+    Friend WithEvents radMale As RadioButton
+    Friend WithEvents radNonBin As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblGPA As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
